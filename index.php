ae40bac6d7233bb99626676b9000e555bd97b250
ahmaddoughman <139462817+ahmaddoughman@users.noreply.github.com>
2024-03-16 23:09:23 +0200

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+  header("location: public/index.php");
+  exit; // Ensure that no further HTML content is sent after the redirection
+?>
+<html>
+  <body>
+    <!-- Any additional HTML content you want to include -->
+  </body>
+</html>
